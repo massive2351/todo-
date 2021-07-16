aa6d6469e926e28491e949c8c4d93620f5380fb5
massive2351 <massive2351@gmail.com>
2021-07-16 03:33:54 +0000

    エラー直し（import漏れ）

--- kanban/views.py
+++ kanban/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import login 
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect 
+
+
 def index(request):
     return render(request, "kanban/index.html")
     

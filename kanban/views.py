f05c601d7a1c8db7f757271f8ba96c244aaee322
massive2351 <massive2351@gmail.com>
2021-07-16 03:07:36 +0000

    top_page templeate

--- kanban/views.py
+++ kanban/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
-
 def index(request):
-    return HttpResponse("仮のトップページ")
+    return render(request, "kanban/index.html")

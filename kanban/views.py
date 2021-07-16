b01af3f5708c6410512c71a26ac27edb514de8bc
massive2351 <massive2351@gmail.com>
2021-07-16 03:27:04 +0000

    サインアップ用のビューの作成

--- kanban/views.py
+++ kanban/views.py
@@ -4,3 +4,19 @@ def index(request):
     
 def home(request):
     return render(request, "kanban/home.html")
+    
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'kanban/signup.html', context)

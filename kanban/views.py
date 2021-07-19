6e85af9509b7022b9343e3a03074d32131a8a676
massive2351 <massive2351@gmail.com>
2021-07-19 07:03:18 +0000

    デコレータ

--- kanban/views.py
+++ kanban/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect 
 
@@ -23,4 +24,23 @@ def signup(request):
     context = {
         "form": form
     }
+    return render(request, 'kanban/signup.html', context)
+    
+@login_required # 追加
+def home(request):
+    return render(request, "kanban/home.html")
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+        context = {
+            "form": form
+        }
     return render(request, 'kanban/signup.html', context)

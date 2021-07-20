3244f68c65f080600f235f385ff57ab84531a957
massive2351 <massive2351@gmail.com>
2021-07-20 04:15:35 +0000

    エラー発生

--- kanban/urls.py
+++ kanban/urls.py
@@ -1,4 +1,4 @@
- 
+
 from django.urls import path
 
 from . import views
@@ -7,6 +7,7 @@ app_name = "kanban"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("home/", views.home, name="home"), 
+    path("home/", views.home, name="home"),
     path('signup/', views.signup, name='signup'),
+    path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
 ]

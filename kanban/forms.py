bf5e98d79597fefc6b359a1fbb43dc5efe9c0e80
massive2351 <massive2351@gmail.com>
2021-07-23 13:47:39 +0000

    リストのフォーム作成

--- kanban/forms.py
+++ kanban/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import List
 
 
 class UserForm(forms.ModelForm):
@@ -8,3 +9,9 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ("username", "last_name", "first_name", "email",)
         
+
+class ListForm(forms.ModelForm):
+
+    class Meta:
+        model = List
+        fields = ("title",)

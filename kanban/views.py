6033fe602fad79d8a9fb652411c9b2927dc0abc4
massive2351 <massive2351@gmail.com>
2021-07-27 06:37:55 +0000

    リストビュー継承継承

--- kanban/views.py
+++ kanban/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect 
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, UpdateView, CreateView
+from django.views.generic import DetailView, UpdateView, CreateView, ListView
 from django.shortcuts import render, redirect, resolve_url 
 from django.views.generic import DetailView, UpdateView 
 
